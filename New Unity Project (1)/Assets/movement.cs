27e9a7150c7ae96b9903a9f7087bcb628e45f6ed
WarPromo <56988049+WarPromo@users.noreply.github.com>
2021-09-06 16:43:37 -0700

    button

--- New Unity Project (1)/Assets/movement.cs
+++ New Unity Project (1)/Assets/movement.cs
@@ -69,7 +69,7 @@ public class movement : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "ground")
         {
---
+++ New Unity Project (1)/Assets/scrollbutton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollbutton : MonoBehaviour
+{
+    bool buttonpressed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collision");
+        if(collision.gameObject.tag == "Player")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+        }
+        if(collision.gameObject.tag == "buttonactivator")
+        {
+            print("START AUTO SCROLL");
+            buttonpressed = true;
+        }
+    }
+}
