c281c6fcdda06638b08e6a47e7fd5bcefad8a9f3
WarPromo <56988049+WarPromo@users.noreply.github.com>
2021-09-06 20:48:56 -0700

    rainbow sword omggggg

---
+++ New Unity Project (1)/Assets/rainb.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rainb : MonoBehaviour
+{
+    SpriteRenderer renderer;
+
+
+    int n = 1;
+    int m = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int b = n % 3;
+        float[] c = { renderer.color.r, renderer.color.g, renderer.color.b };
+
+        if(m == 1 && c[b] >= 1)
+        {
+            n++;
+            b = n % 3;
+            if (c[b] >= 1) m = -1;
+            if (c[b] <= 0) m = 1;
+        }
+        if (m == -1 && c[b] <= 0)
+        {
+            n++;
+            b = n % 3;
+            if (c[b] >= 1) m = -1;
+            if (c[b] <= 0) m = 1;
+
+
+        }
+
+
+
+        c[b] += Time.deltaTime * m;
+
+        print(c[0] + " " + c[1] + " " + c[2]);
+
+
+
+        renderer.color = new Color(c[0], c[1], c[2]);
+
+      
+    }
+}
